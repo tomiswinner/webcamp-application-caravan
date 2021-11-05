fe0e19d6169f0eafb0474397fe2cdc831d2fe179
EC2 Default User <ec2-user@ip-172-31-2-86.ec2.internal>
2021-11-05 00:39:00 +0000

    finish 3-5 appCaravan

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :blogs
   # Prefix Verb   URI Pattern                                                                              Controller#Action
-  #                   blogs GET    /blogs(.:format)                                                                         blogs#index
-  #                         POST   /blogs(.:format)                                                                         blogs#create
-  #               new_blog GET    /blogs/new(.:format)                                                                     blogs#new
-  #               edit_blog GET    /blogs/:id/edit(.:format)                                                                blogs#edit
-  #                   blog GET    /blogs/:id(.:format)                                                                     blogs#show
-  #                         PATCH  /blogs/:id(.:format)                                                                     blogs#update
-  #                         PUT    /blogs/:id(.:format)                                                                     blogs#update
+  #                         POST   /blogs(.:format)                       blogs#create
+  #               new_blog GET    /blogs/new(.:format)                   blogs#new
+  #               edit_blog GET    /blogs/:id/edit(.:format)                  blogs#edit
+  #                   blog GET    /blogs/:id(.:format)                     blogs#show
+  #                         PATCH  /blogs/:id(.:format)                     blogs#update
+  #                         PUT    /blogs/:id(.:format)                   blogs#update
   #                         DELETE /blogs/:id(.:format)
 
 
